085123420ed664235b2b723323e820bf1da27be3
Torkel Ödegaard <torkel.odegaard@gmail.com>
2016-01-13 17:48:28 +0100

    updated

--- public/module.js
+++ public/module.js
@@ -1,6 +1,26 @@
 define([
-], function()  {
+  'angular',
+  'app/app'
+], function(angular, app)  {
 
-  console.log('app-plugin-init');
+  var module = angular.module('nginx-app', []);
+  app.useModule(module);
+
+  module.config(function($routeProvider) {
+    $routeProvider
+      .when('/nginx/stream', {
+        templateUrl: 'public/plugins/nginx-app/partials/stream.html',
+      });
+  });
+
+  function configViewDirective() {
+    return {templateUrl: 'public/plugins/nginx-app/partials/config.html'};
+  }
+
+  return {
+    directives: {
+      configView: configViewDirective
+    }
+  };
 
 });
